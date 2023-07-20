90639ceabc54c1f38fd8d84582475c219aac9924
Gautham2980 <gautham4816@gmail.com>
2023-07-20 13:03:19 +0530

    functionjs

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function mark(mark1,mark2,mark3,mark4,mark5){
+var total=mark1+mark2+mark3+mark4+mark5;
+ 
+document.write("Total Mark:"+total);
+var avg=total/mark.length;
+return(avg);
+}
+
+var Gautham=mark(90,92,78,94,98);
+document.write(" Gautham Average Mark:"+Gautham+"<br>");
+
+var Sakthi=mark(80,72,78,74,88);
+document.write("Sakthi Average Mark:"+Sakthi+"<br>");
+
+var Johnson=mark(90,82,78,94,78);
+document.write("Johnson Average Mark:"+Johnson+"<br>");
+
+var Inian=mark(70,72,78,74,68);
+document.write("Inian Average Mark:"+Inian+"<br>");
+
+var Venkat=mark(90,92,78,94,98);
+document.write("Venkat Average Mark:"+Venkat+"<br>");
+
+
+
+
+
